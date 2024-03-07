3d9c5ca4897b10a86a00600dfd1c41bc9c0ec402
Nick Harrison <nharr@umich.edu>
2024-03-07 14:31:53 -0500

    Jenkins part 2

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,8 +3,8 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 1;
-		int b = 3;
+		int a = 99; //changed to 99
+		int b = 33; // changed to 33
 		int c = 2;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
@@ -13,6 +13,7 @@ public class quraticsolver {
 				System.out.println("The solution for the equation " + a + "x^2 + " + b +"x + " + c +" are");
 				System.out.println((-b+Math.sqrt(b*b-4*a*c))/2*a + " and " + (-b-Math.sqrt(b*b-4*a*c))/2*a);
 				System.out.println("Nick Harrison added to this");
+				System.out.println("Part 2 for Jenkins CI");
 			}
 	}
 
